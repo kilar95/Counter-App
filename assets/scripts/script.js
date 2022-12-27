49dc49f1341b056b8e00434760d4551b094b80a6
Ilaria <ilaria.bertoldi.95@gmail.com>
2022-12-27 14:57:50 +0100

    added box shadows

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -30,19 +30,3 @@ document.addEventListener('keydown', (e) => {
     updateCount(e.key);
 });
 
-
-
-// change the font size based on count to fit bigger numbers
-document.addEventListener("DOMContentLoaded", () => {
-    resizeToFit();
-});
-
-function resizeToFit() {
-    let numLength = display.innerText.length;
-
-    if (numLength >= 4 && numLength <= 6) {
-        display.style.fontSize = '86px';
-    } else if (display.innerText.length > 6) {
-        display.style.fontSize = '48px';
-    }
-}
